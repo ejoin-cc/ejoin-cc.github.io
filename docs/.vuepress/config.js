29a72c810f9ab3abf2675c31207e584bd8f55eba
Yuhang Pan <ejoin.cc@gmail.com>
2025-01-29 00:34:24 +0800

    add framework

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,63 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  head: [['link', { rel: 'icon', href: '/images/logo.jpg' }]],
+
+  title: 'Coding 练级宝典',
+  keywords: ['编程指南','C 语言'],
+
+  theme: defaultTheme({
+    hostname: 'https://ejoin.cc',
+    logo: '/images/logo.jpg',
+
+    home: '/',
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '学习路线',
+        link: '/roadmap/introduction.md',
+        activeMatch: true,
+      },
+      {
+        text: '自托管',
+        link: '/self_host/introduction.md',
+      }
+      
+    ],
+    repo: 'https://github.com/ejoin-cc/CodingBook',
+
+    sidebar: {
+      '/roadmap/': [
+        {
+          text: '前言',
+          collapsible: true,
+          link: 'introduction.md',
+        },
+        {
+          text: '入门',
+          collapsible: true,
+          children: ['beginner.md'],
+        }
+      ],
+      '/self_host/': [
+        'introduction.md',
+        'ddns.md',
+      ],
+    },
+
+    editLink: false,
+    lastUpdated: true,
+    contributors: false,
+
+    prev: '上一页',
+    next: '下一页',
+  }),
+
+  bundler: viteBundler(),
+})
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+tagline: null
+heroImage: /images/logo.jpg
+
+footer: Copyright ©2024-2025 Coding 练级宝典 版权所有
+---
